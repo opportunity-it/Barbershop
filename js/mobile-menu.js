ecb2e5635b115911b8b1af57d76e7122641729b8
YuraIvashkiv <yuraivashkiv1995@gmail.com>
2024-09-01 18:13:03 +0300

    fixed header for adaptive layout

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -13,12 +13,12 @@
         ? 'disableBodyScroll'
         : 'enableBodyScroll';
       bodyScrollLock[scrollLockMethod](document.body);
+    // БЛОКУВАННЯ - РОЗБЛОКУВАННЯ ПРОКРУТКИ ПРИ ВІДКРИТОМУ МОБІЛЬНОМУ МЕНЮ 
     };
   
     openMenuBtn.addEventListener('click', toggleMenu);
     closeMenuBtn.addEventListener('click', toggleMenu);
   
-    // Close the mobile menu on wider screens if the device orientation changes
     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
       if (!e.matches) return;
       mobileMenu.classList.remove('is-open');
