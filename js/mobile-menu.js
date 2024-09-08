7c4cc9a44234564b30332feec63784bd8d621319
YuraIvashkiv <yuraivashkiv1995@gmail.com>
2024-09-08 23:43:20 +0300

    changed mobile-menu (fixed)

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,28 +1,38 @@
-(() => {
-    const mobileMenu = document.querySelector('.js-menu-container');
-    const openMenuBtn = document.querySelector('.js-open-menu');
-    const closeMenuBtn = document.querySelector('.js-close-menu');
+// (() => {
+//     const mobileMenu = document.querySelector('.js-menu-container');
+//     const openMenuBtn = document.querySelector('.js-open-menu');
+//     const closeMenuBtn = document.querySelector('.js-close-menu');
   
-    const toggleMenu = () => {
-      const isMenuOpen =
-        openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-      mobileMenu.classList.toggle('is-open');
+//     const toggleMenu = () => {
+//       const isMenuOpen =
+//         openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+//       openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+//       mobileMenu.classList.toggle('is-open');
   
-      const scrollLockMethod = !isMenuOpen
-        ? 'disableBodyScroll'
-        : 'enableBodyScroll';
-      bodyScrollLock[scrollLockMethod](document.body);
-    // БЛОКУВАННЯ - РОЗБЛОКУВАННЯ ПРОКРУТКИ ПРИ ВІДКРИТОМУ МОБІЛЬНОМУ МЕНЮ 
-    };
+//       const scrollLockMethod = !isMenuOpen
+//         ? 'disableBodyScroll'
+//         : 'enableBodyScroll';
+//       bodyScrollLock[scrollLockMethod](document.body);
+//     // БЛОКУВАННЯ - РОЗБЛОКУВАННЯ ПРОКРУТКИ ПРИ ВІДКРИТОМУ МОБІЛЬНОМУ МЕНЮ 
+//     };
   
-    openMenuBtn.addEventListener('click', toggleMenu);
-    closeMenuBtn.addEventListener('click', toggleMenu);
+//     openMenuBtn.addEventListener('click', toggleMenu);
+//     closeMenuBtn.addEventListener('click', toggleMenu);
   
-    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-      if (!e.matches) return;
-      mobileMenu.classList.remove('is-open');
-      openMenuBtn.setAttribute('aria-expanded', false);
-      bodyScrollLock.enableBodyScroll(document.body);
-    });
-  })();
+//     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+//       if (!e.matches) return;
+//       mobileMenu.classList.remove('is-open');
+//       openMenuBtn.setAttribute('aria-expanded', false);
+//       bodyScrollLock.enableBodyScroll(document.body);
+//     });
+//   })();
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.header-burger').addEventListener('click', function() {
+    document.querySelector('.header-burger').classList.toggle('active');
+    document.querySelector('.menu-container').classList.toggle('active');
+    document.body.classList.toggle('no-scroll');
+  });
+});
