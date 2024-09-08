2fa02669f337661eb947c91d4bf466beb9acf808
YuraIvashkiv <yuraivashkiv1995@gmail.com>
2024-09-09 00:08:54 +0300

    added js code for: mobile-menu, header(blur header without scrolling page)

--- js/header.js
+++ js/header.js
@@ -1,3 +1,18 @@
+// window.onscroll = () => changeHeaderBackground();
+
+// function changeHeaderBackground() {
+//   const header = document.querySelector('[data-header]');
+//   const headerOffsetTrigger = header.offsetHeight;
+//   const pageOffset = window.pageYOffset;
+
+//   if (pageOffset > headerOffsetTrigger) {
+//     header.classList.add('no-transparency');
+//   } else {
+//     header.classList.remove('no-transparency');
+//   }
+// }
+
+
 window.onscroll = () => changeHeaderBackground();
 
 function changeHeaderBackground() {
@@ -5,9 +20,13 @@ function changeHeaderBackground() {
   const headerOffsetTrigger = header.offsetHeight;
   const pageOffset = window.pageYOffset;
 
-  if (pageOffset > headerOffsetTrigger) {
+  // Додаємо перевірку, чи меню активне
+  const menuIsActive = document.querySelector('.menu-container').classList.contains('active');
+
+  if (pageOffset > headerOffsetTrigger || menuIsActive) {
     header.classList.add('no-transparency');
   } else {
     header.classList.remove('no-transparency');
   }
 }
+// Щоб було розмиття хедера без прокрутки сторінки
--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -29,10 +29,30 @@
 
 
 
+// document.addEventListener('DOMContentLoaded', function() {
+//   document.querySelector('.header-burger').addEventListener('click', function() {
+//     document.querySelector('.header-burger').classList.toggle('active');
+//     document.querySelector('.menu-container').classList.toggle('active');
+//     document.body.classList.toggle('no-scroll');
+//   });
+// });
+
+
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.header-burger').addEventListener('click', function() {
-    document.querySelector('.header-burger').classList.toggle('active');
-    document.querySelector('.menu-container').classList.toggle('active');
+  const burger = document.querySelector('.header-burger');
+  const menuContainer = document.querySelector('.menu-container');
+  const header = document.querySelector('[data-header]');
+
+  burger.addEventListener('click', function() {
+    burger.classList.toggle('active');
+    menuContainer.classList.toggle('active');
     document.body.classList.toggle('no-scroll');
+
+    if (menuContainer.classList.contains('active')) {
+      header.classList.add('no-transparency');
+    } else {
+      header.classList.remove('no-transparency');
+    }
   });
 });
+// Щоб було розмиття хедера без прокрутки сторінки
