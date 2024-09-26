3fa40b97320cbc58033369cfbfa8873af20ad053
YuraIvashkiv <yuraivashkiv1995@gmail.com>
2024-09-26 19:51:27 +0300

    fixed modal window (js)

--- js/modal.js
+++ js/modal.js
@@ -22,25 +22,28 @@
 
 
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      const isModalOpen = refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
-      refs.openModalBtn.setAttribute('aria-expanded', !isModalOpen);
-      refs.modal.classList.toggle('is-hidden');
-  
-      const scrollLockMethod = !isModalOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-      bodyScrollLock[scrollLockMethod](document.body);
-    }
-  })();
-  
+  const refs = {
+    openModalBtns: document.querySelectorAll('[data-modal-open]'), // Змінили на querySelectorAll
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtns.forEach(btn => {
+    btn.addEventListener('click', toggleModal);
+  });
+
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    const isModalOpen = refs.modal.classList.contains('is-hidden');
+    
+    refs.modal.classList.toggle('is-hidden');
+    
+    const scrollLockMethod = isModalOpen ? 'disableBodyScroll' : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  }
+})();
+
   
   
   
